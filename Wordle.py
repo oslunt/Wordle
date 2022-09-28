a53581e4bbe1fd1b65f4b66155e8409fd19c1dc7
oslunt <77417770+oslunt@users.noreply.github.com>
2022-09-28 11:45:45 -0600

    Milestone 1

--- Wordle.py
+++ Wordle.py
@@ -11,14 +11,18 @@ from WordleDictionary import FIVE_LETTER_WORDS
 from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
 
 def wordle():
-    word = random.choice(FIVE_LETTER_WORDS)
-
+    
     def enter_action(s):
-        gw.set_current_row(s)
+
         gw.show_message(s)
 
     gw = WordleGWindow()
     gw.add_enter_listener(enter_action)
+    
+    word = random.choice(FIVE_LETTER_WORDS)
+    for i in range(0,len(word)):
+        gw.set_square_letter(0, i, word[i])
+
 
 # Startup code
 
